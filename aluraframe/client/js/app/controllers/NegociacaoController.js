0c72fe1ea3bd31b2a048d0892ac5f58b6e4c7060
Paulo Lirio de Oliveira Pereira <paulo.lirio@PC-079.impa.br>
2019-04-11 17:08:48 -0300

    Services

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,24 +4,44 @@ class NegociacaoController {
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
-        this._listaNegociacoes = new ListaNegociacoes();
 
-        this._negociacoesView = new NegociacoesView($("#negociacoesView"));
-        this._negociacoesView.update(this._listaNegociacoes);
-        this._mensagem = new Mensagem();
-        this._mensagemView = new MensagemView($("#mensagemView"));
-        this._mensagemView.update(this._mensagem);
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(),
+            new NegociacoesView($("#negociacoesView")),
+            "adiciona", 
+            "esvazia");
+        
+        this._mensagem = new Bind(
+            new Mensagem(),  
+            new MensagemView($("#mensagemView")),
+            "texto");
     }
 
     adiciona(event) {
         event.preventDefault();
         
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
-
         this._mensagem.texto = "Negociação adicionada com sucesso";
-        this._mensagemView.update(this._mensagem);
         this._limpaFormulario();
+    }
+
+    importaNegociacoes() {
+        let service = new NegociacaoService();
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if (erro) {
+                this._mensagem.texto = erro;
+                return;
+            }
+
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = "Negociações importadas com sucesso";
+        });
+
+    }
+
+    apaga() {
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = "Negociações apagadas com sucesso";
     }
 
     _criaNegociacao() {
--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -4,11 +4,19 @@ class ListaNegociacoes {
     this._negociacoes = [];
   }
 
-  adiciona(negociacoes) {
-    this._negociacoes.push(negociacoes);
+  adiciona(negociacao) {
+    this._negociacoes.push(negociacao);
   }
 
   get negociacoes() {
     return [].concat(this._negociacoes);
+  }
+
+  esvazia() {
+    this._negociacoes = [];
+  }
+
+  get volumeTotal() {
+    return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
   }
 }
