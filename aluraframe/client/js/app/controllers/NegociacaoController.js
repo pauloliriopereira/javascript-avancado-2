78425a7dd11c66ce08075d4549b9640b641ce43c
paulolirio <paulolirio@n46v.schedulesoftware>
2019-04-12 00:57:11 -0300

    ajax

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -27,16 +27,15 @@ class NegociacaoController {
 
     importaNegociacoes() {
         let service = new NegociacaoService();
-        service.obterNegociacoesDaSemana((erro, negociacoes) => {
-            if (erro) {
-                this._mensagem.texto = erro;
-                return;
-            }
 
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = "Negociações importadas com sucesso";
-        });
-
+        Promise.all([service.obterNegociacoesDaSemana(),
+                     service.obterNegociacoesDaSemanaAnterior(),
+                     service.obterNegociacoesDaSemanaRetrasada()]
+        ).then(negociacoes => {
+            negociacoes.reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+        })
+        .catch(erro => this._mensagem.texto = erro);
     }
 
     apaga() {
